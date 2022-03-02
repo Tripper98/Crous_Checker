1c07f0040c0c7c1d867de1a2bae8a55b7ec58618
DRIOUCHE Adnane <adnanedriouche98@gmail.com>
2022-03-02 16:23:19 +0100

    changing time

--- scrap.py
+++ scrap.py
@@ -10,13 +10,13 @@ while True :
     total = response_paris.json()["results"]["total"]
     jprint(total)
 
-    if total >= 0 : 
+    if total > 0 : 
         
         ## b = "https://trouverunlogement.lescrous.fr/tools/flow/21/search?bounds=-0.6387_44.9162_-0.5337_44.8108&page=1&price=60000"
         url = "https://trouverunlogement.lescrous.fr/tools/flow/21/search?bounds=2.2163_48.9716_2.4781_48.7458&page=1&price=60000"
 
-        webbrowser.open(url, new= 2, autoraise=True)
+        webbrowser.open(url, new= 0, autoraise=True)
 
-    time.sleep(5)
+    time.sleep(300)
 
 
